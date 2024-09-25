6b52fdbf284b34dc3c7235b169e874c677904990
unknown <devaman2508@gmail.com>
2024-09-25 09:03:57 +0530

    added footer and slider in frontpage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,30 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
 import Layout from "./Layout"
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 const Home =()=>{
     return(
         <Layout>
         <div>
-            <h1>HOme</h1>
+            <header>
+            <Swiper
+             navigation={true} 
+             pagination={{
+                dynamicBullets: true,
+              }}
+             modules={[Navigation,Pagination]}
+             slidesPerView={1} >
+      <SwiperSlide><img src="/images/a.jpg" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="/images/b.jpg" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="/images/c.jpg" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="/images/d.png" alt="" /></SwiperSlide>
+    
+    </Swiper>
+            </header>
         </div>
         </Layout>
     )
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -30,7 +30,7 @@ return(
         <ul className="flex gap-4 items-center">
             {
             menus.map((list,index)=>(
-                <li key={list.index}>
+                <li key={index}>
                 <Link to={list.href}
                 className="block py-6 text-center hover:bg-blue-600 w-[100px] hover:text-white" 
                 >
@@ -47,7 +47,7 @@ return(
                 Login
             </Link>
             <Link 
-            className=" bg-blue-600 py-3 px-6 text-md font-semibold rounded text-white block text-center hover:bg-blue-600 w-[100px] hover:text-white" 
+            className=" bg-blue-600 py-3 px-6 text-md font-semibold rounded text-white block text-center hover:bg-rose-600 w-[100px] hover:text-white" 
             to='/signup'
             >
                 Signup
@@ -58,17 +58,87 @@ return(
         </div>
 
        </nav>
+       <div>
+        {children}
+       </div>
 
        <footer className="bg-orange-600 p-12">
-       <div className="w-10/12 mx-auto grid grid-cols-3">
+       <div className="w-10/12 mx-auto grid grid-cols-4">
        <div>
+        <h1 className="text-white font-semibold text-2xl mb-3">Follow Us</h1>
+       <ul className="space-y-2 text-slate-50">
+       
+        <li><Link to='/'>Facebook</Link></li>
+        <li><Link to='/'>Youtube</Link></li>
+        <li><Link to='/'>Linkedin</Link></li>
+        <li><Link to='/'>Instagram</Link></li>
+        <li><Link to='/'>Twiter</Link></li>
+       
+       </ul>
+      
+       </div>
+       
+       <div>
+        <h1 className="text-white font-semibold text-2xl mb-3">Website Links</h1>
+       <ul className="space-y-2 text-slate-50">
+        {
+            menus.map((item,index)=>(
+
+            <li key={index}>
+                <Link to={item.href}>
+                {item.label}
+                </Link>
+            </li>
+
+            ))
+        }
+        <li><Link to='/login'>Login</Link></li>
+        <li><Link to ='/signup'>Signup</Link></li>
+       </ul>
+      
+       </div>
+       <div className="pr-8">
+        <h1 className="text-white font-semibold text-2xl mb-3">Brand Details</h1>
+       <p className="text-justify text-slate-50 mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, Ducimus, neque magni.  </p>
        <img src='images/logo.jpg'
         className="w-24"
         />
-        <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, fugiat. Amet perspiciatis doloribus explicabo ducimus maxime corporis quae consectetur quasi magni aliquid expedita, delectus provident ipsam reprehenderit? Ducimus, neque magni.  </p>
+      
        </div>
-       <div>2</div>
-       <div>3</div>
+
+    
+
+    
+
+       <div>
+        <h1 className="text-white font-semibold text-2xl mb-3">Conatct us
+        </h1>
+      <form className="space-y-4">
+        <input
+        name="fullname"
+        className="bg-white w-full rounded p-3"
+        placeholder = "Your Name"
+        required
+        />
+        <input
+        type="email"
+        name="email"
+        className="bg-white w-full rounded p-3"
+        placeholder = "Enter E-mail"
+        required
+        />
+        <textarea
+        required
+        name="message"
+        className="bg-white w-full rounded p-3"
+        placeholder="Message"
+        rows={3}
+        />
+        <button className="bg-black text-white py-3 px-6 rounded">Submit</button>
+      </form>
+      
+       </div>
+       
 
        </div>
 
