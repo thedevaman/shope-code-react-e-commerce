c9fdedf1fe32c598c45f206a4ad59b87f48ea4bd
unknown <devaman2508@gmail.com>
2024-10-17 08:58:24 +0530

    profile image updated

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,26 +1,23 @@
 import { useEffect, useState } from "react"
 import firebaseAppConfig from "../util/firebase-config"
 import Layout from "./Layout"
-import { onAuthStateChanged,getAuth, } from "firebase/auth"
-import { getStorage, ref, uploadBytes } from "firebase/storage"
+import { onAuthStateChanged,getAuth, updateProfile, } from "firebase/auth"
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage"
 import { useNavigate } from "react-router-dom"
+import Swal from "sweetalert2"
 
 const auth = getAuth(firebaseAppConfig)
 const storage = getStorage()
 
 
 const Profile = ()=>{
+    const [uploading,setUploading] = useState(false)
     const navigate = useNavigate()
     const [session,setSession] = useState(null)
     const [formvalue, setFormValue] = useState({
         fullname:'',
         email:'',
-        mobile:'',
-        address:'',
-        city:'',
-        state:'',
-        country:'' ,
-        pincode:''
+        mobile:''
     })
    useEffect(()=>{
     onAuthStateChanged(auth,(user)=>{
@@ -34,6 +31,17 @@ const Profile = ()=>{
     })
    },[])
 
+   useEffect(()=>{
+      if(session)
+      {
+        setFormValue({
+            ...formvalue,
+            fullname:session.displayName,
+            mobile:(session.phoneNumber?session.phoneNumber:'')
+        })
+      }
+   },[session])
+
    const setProfilePicture = async (e)=>{
       const input = e.target
       const file = input.files[0]
@@ -42,9 +50,17 @@ const Profile = ()=>{
       const filename = Date.now()+'.'+ext
       const path = `profile_pic/${filename}`
       const bucket = ref(storage,path)
+      setUploading(true)
       const snapshot = await uploadBytes(bucket, file)
-      const imageurl = `https://firebasestorage.googleapis.com/v0/b/shopecode-ba1d7.appspot.com/o/${path}`
-      console.log(imageurl)
+      const url = await getDownloadURL(snapshot.ref)
+      await updateProfile(auth.currentUser,{
+        photoURL:url
+      })
+      setUploading(false)
+      setSession({
+        ...session,
+        photoURL:url
+      })
      
    }
 
@@ -63,7 +79,18 @@ const Profile = ()=>{
 <div>Loading...</div>
     )
 
+const saveprofileinfo = async (e)=>{
+  e.preventDefault()
+  await updateProfile(auth.currentUser,{
+    displayName:formvalue.fullname,
+    phoneNumber:formvalue.mobile
+  })
 
+  new Swal({
+    icon:'success',
+    title:'Profile Updated'
+  })
+}
 
 
 return(
@@ -75,17 +102,23 @@ return(
         </div>
         <hr className="my-6" />
         <div className="w-24 h-24 mx-auto relative mb-6">
-        <img src="/images/avt.webp" className="rounded-full border w-24 h-24" />
+        {
+            uploading?
+            <img src="/images/loader.gif"/>
+            :
+            <img src={session.photoURL?session.photoURL:"/images/avt.webp"} className="rounded-full border w-24 h-24" />
+        }
+        
         <input type="file" accept="image/*" className="opacity-0 absolute top-0 left-0 w-full h-full" onChange={setProfilePicture} />
         </div>
-        <form className="grid grid-cols-2 gap-6">
+        <form className="grid grid-cols-2 gap-6" onSubmit ={saveprofileinfo}>
            
             <div className="grid grid-col gap-2">
             <label className="text-lg font-semibold">Full name</label>
             <input 
             name="fullname"
             className="p-2 rounded border border-gray-300"
-            value={session.displayName}
+            value={formvalue.fullname}
             required
             onChange={handleForm}
             />
@@ -93,6 +126,7 @@ return(
             <div className="grid grid-col gap-2">
             <label className="text-lg font-semibold">Email</label>
             <input 
+            disabled
             name="email"
             type="email"
             className="p-2 rounded border border-gray-300"
@@ -115,62 +149,7 @@ return(
             <div />
 
 
-            <div className="grid grid-col gap-2 col-span-2">
-            <label className="text-lg font-semibold">Area/Street/Village</label>
-            <input 
-            name="address"
-            type="text"
-            className="p-2 rounded border border-gray-300"
-            value={formvalue.address}
-            required
-            onChange={handleForm}
-            />
-            </div>
-
-            <div className="grid grid-col gap-2">
-            <label className="text-lg font-semibold">City</label>
-            <input 
-            name="city"
-            type="text"
-            className="p-2 rounded border border-gray-300"
-            value={formvalue.city}
-            required
-            onChange={handleForm}
-            />
-            </div>
-            <div className="grid grid-col gap-2">
-            <label className="text-lg font-semibold">State</label>
-            <input 
-            name="state"
-            type="text"
-            className="p-2 rounded border border-gray-300"
-            value={formvalue.state}
-            required
-            onChange={handleForm}
-            />
-            </div>
-            <div className="grid grid-col gap-2">
-            <label className="text-lg font-semibold">Country</label>
-            <input 
-            name="country"
-            type="text"
-            className="p-2 rounded border border-gray-300"
-            value={formvalue.country}
-            required
-            onChange={handleForm}
-            />
-            </div>
-            <div className="grid grid-col gap-2">
-            <label className="text-lg font-semibold">Pincode</label>
-            <input 
-            name="pincode"
-            type="number"
-            className="p-2 rounded border border-gray-300"
-            value={formvalue.pincode}
-            required
-            onChange={handleForm}
-            />
-            </div>
+         
             <button className="px-4 py-2 bg-rose-600 text-white rounded w-fit hover:bg-green-600">
                 <i className="ri-save-line mr-2"></i>
                 Save
